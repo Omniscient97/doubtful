9f263f91399edc818a42dcb3b770069b6005f03d
Joshua Rowe <joshuarowe@Joshuas-MacBook-Air.local>
2013-12-15 01:06:14 +1100

    updated print to print()

--- lexicon.py
+++ lexicon.py
@@ -1,5 +1,5 @@
-print "Enter commands into the prompt."
+print('Enter commands into the prompt.')
 user_input = input("> ")
 words = user_input.split()
-print words
-input("press enter to exit ;)")
+print(words)
+input('press enter to exit ;)')
