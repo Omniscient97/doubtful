80dd36ad69f852f59e96731b68b6c9e773bd57e1
Joshua Rowe <joshuarowe@Joshuas-MacBook-Air.local>
2013-12-15 22:32:26 +1100

    added a word bank and word type analysis

--- lexicon.py
+++ lexicon.py
@@ -1,5 +1,16 @@
 print('Enter commands into the prompt.')
-user_input = input("> ")
+
+lexicon = {'go': 'verb', 'open': 'verb', 'north': 'direction', 'south': 'direction',
+'east': 'direction', 'west': 'direction'}
+
+
+user_input = input('> ')
 words = user_input.split()
-print(words)
+output_words = []
+for word in words:
+	if word in lexicon:
+		output_words.append((word, lexicon[word]))
+	else:
+		output_words.append(word, 'error')
+print(output_words)
 input('press enter to exit ;)')
