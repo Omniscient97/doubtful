78f4b82820242110b6400f1055256c1519bdcbe3
Joshua Rowe <joshuarowe1@icloud.com>
2013-12-16 16:06:55 +1100

    added game loop with basic error handling

--- doubtful/game.py
+++ doubtful/game.py
@@ -9,6 +9,12 @@ player = Player()
 
 player.name = input('Please input your name: ')
 print("hello,", player.name)
-user_input = input('enter commands: ')
-print(lexicon.scan(user_input))
-input('press enter to exit ;)')
+print('Type "exit" to quit the game')
+game = True
+while game:
+	user_input = lexicon.scan(input('enter commands: '))
+	for word in user_input:
+		if word[1] == "exit":
+			game = False
+		if word[0] == "error":
+			print('I dont understand "%s"' % word[1])
