edc72a9ab7a98f92865c791db19c1ed9f77e7b86
Joshua Rowe <joshuarowe1@icloud.com>
2013-12-17 18:39:29 +1100

    added test files and added /help to README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 doubtful
 ========
+
+Commands:
+/help - lists basic verbs and game commands
---
+++ tests/inventory_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import doubtful.inventory
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
